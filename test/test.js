42e2b732c7bb5f6afeb7a2bb63af08465867bdf0
Natalia Reshetnikova <natalie.reshetnikova@gmail.com>
2017-03-26 03:41:57 +0800

    commit

---
+++ test/test.js
@@ -0,0 +1,33 @@
+var chai = require('chai');
+var should = chai.should();
+var chaiAsPromised = require('chai-as-promised');
+
+
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+const mochaTimeOut = 35000;
+
+describe('Link "Софт для быстрого создания скриншотов"', function () {
+    var driver;
+    before(function(){
+        driver = new webdriver.Builder().
+        withCapabilities(webdriver.Capabilities.chrome()).
+        build();
+        chai.use(chaiAsPromised);
+    });
+
+    after(function(){
+        driver.quit();
+    });
+
+    it('should lead to "http://monosnap.com"', function (done) {
+        var url = 'http://blog.csssr.ru/qa-engineer';
+        var expected_links = ['http://monosnap.com/', 'http://monosnap.com', 'http://app.prntscr.com/ru/'];
+
+        driver.get(url);
+        driver.findElement(By.css('label[for=soft]>a')).getAttribute('href')
+            .should.eventually.be.oneOf("http://monosnap.com").notify(done);
+    });
+});
